66f0bd10cd11d0f2789ea3c3bab015dbff3aff4b
gingergerpan <100305067@nccu.edu.tw>
2016-01-11 00:51:19 +0800

    comment 前端appendTo

---
+++ javascripts/comment.js
@@ -0,0 +1,15 @@
+(function(){
+    $('#comment').on('click',function(){
+    	var commentContent = $('textarea').val();
+    	if (commentContent) {
+	    	$("<div class='past-comment only-shadow' id='new'></div> ").appendTo('.comment');
+	    	$("<img src='images/photo.png'><div class='comment-content'></div>").appendTo('#new');
+	    	$(
+	    	"<p class='user-name'>"+ "我的名字"+"</p>"+
+	    	"<p class='post-time'>"+ "發布的時間"+"</p>"+
+	    	"<p>"+ commentContent + "</p>"
+	    	).appendTo("#new .comment-content");
+    	}
+
+    });
+})();
